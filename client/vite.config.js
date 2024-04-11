6e73034aeff8a96cbbb6c4cde78c6299ec9feb4c
ferewar <73423237+ferewar@users.noreply.github.com>
2024-04-10 19:50:23 -0700

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,12 @@
-// Import the necessary functions and plugins for configuration.
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Add any Vite plugins used in the project.
   plugins: [react()],
-  
-  // Configuration for the development server.
   server: {
-    port: 3000, // Specifies the port on which the development server will run.
-    open: true, // Tells the server to automatically open the app in the browser when started.
+    port: 3000, 
+    open: true, 
     
-    //Define proxies for API requests made from the client. Useful in development to avoid CORS issues.
     proxy: {
       '/graphql': { 
         target: 'http://localhost:3001', 
